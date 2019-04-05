86053ee632ec5dc1473ce954c4f16d23945ffa1c
Julian Purukan <julianpurukan1@gmail.com>
2019-04-05 12:43:31 +0700

    edit rest application ke code original

--- src/main/java/com/mycompany/app/rest/RestApplication.java
+++ src/main/java/com/mycompany/app/rest/RestApplication.java
@@ -3,6 +3,6 @@ package com.mycompany.app.rest;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ApplicationPath;
 
-@ApplicationPath("/rest/api1")
+@ApplicationPath("/rest")
 public class RestApplication extends Application {
 }
